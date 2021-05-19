717a1d710f1de141d870fc76244883829f43f5b0
mikhawa <michaeljpitz@gmail.com>
2021-05-19 10:45:22 +0100

    01-hello-world.php 2

--- 01-hello-world.php
+++ 01-hello-world.php
@@ -3,4 +3,5 @@
 // echo permet d'afficher une chaîne de caractère
 echo "Hello World";
 
-// fermeture de PHP => ?>
+// fermeture de PHP => ? >
+// cette balise est inutile si aucun texte après la balise (bonnes pratiques) ?>
