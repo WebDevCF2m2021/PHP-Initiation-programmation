7085385a438bfa1e98d5873ec910c6079dc7f423
mikhawa <michaeljpitz@gmail.com>
2021-05-19 11:02:53 +0100

    PHP with html

---
+++ 02-hello-world-with-html.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title><?php echo "Hello world !!!"; ?></title>
+    </head>
+    <body>
+        <?php
+        echo "<h1>Hello world</h1>";
+        ?>
+    </body>
+</html>
