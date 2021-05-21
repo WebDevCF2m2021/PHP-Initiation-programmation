2fd8dd2ba27408bd263a2fbac651fd4a851d58a0
mikhawa <michaeljpitz@gmail.com>
2021-05-21 09:03:08 +0100

    07

---
+++ 07-comment-multi.php
@@ -0,0 +1,8 @@
+<?php
+echo "commentaire sur plusieures lignes /* */<br>";
+/*
+
+Ici on met un commentaire de la 
+longueur souhaîtée sur plusieures lignes
+
+*/echo "Cette balise n'est pas auto-fermante!";
