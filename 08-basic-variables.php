6b878b3aa851d9c242b09f9230de75ca5191e6ff
mikhawa <michaeljpitz@gmail.com>
2021-05-21 10:59:22 +0100

    08

--- 08-basic-variables.php
+++ 08-basic-variables.php
@@ -1,3 +1,24 @@
 <?php
+// déclaration de variables
+$name = "Pitz"; // string
+$surname = 'Michaël'; // string
+$age = 44; // integer (number)
+$height = 170.2; // float (number)
 
+// affichage des variables avec echo, si entre "", les variables sont interpétées, ce n'est pas le cas entre '' !
+echo "<p>Mon nom est $name $surname et je suis âgé de $age ans</p>"; // affichage d'une string
 
+// pas d'interprétation entre simple ''
+echo '<p>Mon nom est $name $surname et je suis âgé de $age ans</p>';
+
+// concaténation avec . ou ,
+echo "<p>Mon nom est ".$name.' ',$surname." et je suis âgé de ".$age.' ans</p>';
+
+// erreur, ne pas commencer par un chiffre
+// $45bcd = "lala";
+
+// erreur, pas d'espaces
+// $lala lulu = 25;
+
+// pasq d'erreur, mais à éviter !
+$éçeãö§çcole = 25;
