7780dab45e86817a1e7bed106537d44f21eb076d
mikhawa <michaeljpitz@gmail.com>
2021-05-21 08:40:05 +0100

    05

---
+++ 05-semicone.php
@@ -0,0 +1,26 @@
+<?php
+// La seul fin d'instruction est le ;
+    echo "A";
+    
+    
+    
+    
+                    echo                                        
+    
+    "B"
+    
+    
+    
+    
+                ;
+    
+    echo            "C";
+    
+    echo "ABC";
+        // concaténation averc le .
+	echo "A"."B"."C";
+        // concaténation averc le ,
+	echo "A","B","C";
+        // les () ne sont plus obligatoires (fonction qui est devenue une structure du langage)
+	echo("ABC");
+
