1246b3d7fb80d3edb0eb9bfcb2f7db9d183bff51
mikhawa <michaeljpitz@gmail.com>
2021-05-28 09:03:15 +0100

    14

---
+++ 14-assignement-operators.php
@@ -0,0 +1,29 @@
+<?php
+// variables numériques
+$x=5;$y=2;
+
+// +=
+$x+=$y;
+echo $x;
+echo "<hr>";
+
+// -=
+$x-=$y;
+echo $x;
+echo "<hr>";
+
+// *=
+$x*=$y;
+echo $x;
+echo "<hr>";
+
+// /=
+$x/=$y;
+echo $x;
+echo "<hr>";
+
+// %=
+// *=
+$x%=$y;
+echo $x;
+echo "<hr>";
